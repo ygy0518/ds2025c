948dbab39ec3d9a19ec04a21c4ff7c07f5fcdc1a
unknown <ygy0518>
2025-03-17 15:40:04 +0900

    Guess game

--- week02.py
+++ week02.py
@@ -1,4 +1,17 @@
-# alt + shift + F10
-n = int(input("정수 입력: "))
-result = n * (n+1) // 2  # O(1)
-print(result)
+import random
+
+random_number = random.randint(1, 100)
+win = False
+
+for guesses in range(7):
+    print(f"{7-guesses}번 기회 남음. 숫자 입력 : ", end="")
+    guess = int(input())
+
+    if random_number == guess:
+        print("정답")
+        win = True
+        break
+    elif random_number < guess:
+        print("더 작아")
+    else:
+        print("더 커")
